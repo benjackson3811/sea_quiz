e72f4664152caeb869646ee6d0dc7930e5640acf
benjackson3811 <benjackson3811@googlemail.com>
2023-03-06 13:17:32 +0000

    Adding questions and options

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+#-----------------------
+def new_game():
+    pass
+#-----------------------
+def check_answer():
+    pass
+#-----------------------
+def display_score():
+    pass
+#-----------------------
+def play_again():
+    pass
+#-----------------------
+
+# dictionary created for the questions
+
+questions = {
+    "What percentage of the Earth's surface is covered by Oceans?: ": "A",
+    "What is the percentage of the planet's oceans that have been explored? ": "A",
+    "Which one the following islands is not in the Atlantic Ocean? ": "C",
+    "Which of the following oceans is the smallest and the shallowest? ": "D",
+    "The Pacific is home to the deepest known trench on the planet - what's its name? ": "A"
+}
+
+options = [["A. 20%", "B. 40%","C. 50%","D. 70%"],
+           ["A. 5%", "B. 25%", "C. 50%", "D. 75%"],
+           ["A. The Bahanmas", "B. Azores", "C, Madagascar", "D, Canarary Islands"],
+           ["A, Atlantic", "B. Pacific", "C. Indian", "D. Arctic"],
+           ["A. Mariana Trench", "B. Kermadec Trench", "C. South Sandwich Trench", "D. Izu-Ogasawara Trench"]]
